7326f254e644367e5bf5cf993775c4903b310676
JyotiKumari1277-ux <jyotikumarirs8709@gmail.com>
2026-06-25 20:42:15 +0530

    enable cors

--- form-builder-api/src/main.ts
+++ form-builder-api/src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+  // src/main.ts file mein ye change karein
+const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT || 3001);
+// Ye line add ya update karein
+app.enableCors({
+  origin: '*', // Iska matlab hai ki koi bhi frontend aapke backend ko call kar sakta hai
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
+
+await app.listen(process.env.PORT || 3001);
   console.log('Backend chal raha hai port 3001 par!');
 }
 bootstrap();
