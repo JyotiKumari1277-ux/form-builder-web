2f6245178466589654504f186009fe551ab373ca
JyotiKumari1277-ux <jyotikumarirs8709@gmail.com>
2026-06-26 08:42:53 +0530

    Fix CORS

--- form-builder-api/src/main.ts
+++ form-builder-api/src/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // src/main.ts file mein ye change karein
-const app = await NestFactory.create(AppModule);
-
-// Ye line add ya update karein
-app.enableCors({
-  origin: '*', // Iska matlab hai ki koi bhi frontend aapke backend ko call kar sakta hai
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
-
-await app.listen(process.env.PORT || 3001);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://form-builder-web-seven.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT || 3001);
   console.log('Backend chal raha hai port 3001 par!');
 }
 bootstrap();
